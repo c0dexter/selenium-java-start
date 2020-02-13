be30ae03ba8f726069f7dc2e545a75bbe220d606
c0dexter <michal.dobrowolski1@gmail.com>
2020-02-13 23:03:23 +0100

    Refacotored code: used WebElement object type instead of repeating findElemmentBy

--- src/test/java/FirstAutomatedTest.java
+++ src/test/java/FirstAutomatedTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,8 +23,10 @@ public class FirstAutomatedTest {
     public void myFirstTest() {
         driver.navigate().to("http://google.pl");
 
-        driver.findElement(By.name("q")).sendKeys("JavaStart");
-        driver.findElement(By.name("q")).submit();
+        WebElement queryField = driver.findElement(By.name("q"));
+
+        queryField.sendKeys("JavaStart");
+        queryField.submit();
 
         String pageTitle = driver.getTitle();
 
