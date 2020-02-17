bba07bee1e254b76d03966a41ade9e3533e8e67e
c0dexter <michal.dobrowolski1@gmail.com>
2020-02-17 22:10:33 +0100

    Added class and test for testing CheckBoxes (Zadanie 7.1)

---
+++ src/test/java/CheckBoxesTests.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CheckBoxesTests {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome_v80/chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://theinternet.przyklady.javastart.pl/checkboxes");
+    }
+
+    @Test
+    public void checkboxesTest(){
+        WebElement checkbox1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+
+        assertFalse(checkbox1.isSelected());
+        assertTrue(checkbox2.isSelected());
+
+        checkbox1.click();
+        checkbox2.click();
+
+        assertTrue(checkbox1.isSelected());
+        assertFalse(checkbox2.isSelected());
+
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
