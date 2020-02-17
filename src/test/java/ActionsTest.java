f0795e4b7ccf5f0e47220a9864ff0d85baf4219f
c0dexter <michal.dobrowolski1@gmail.com>
2020-02-17 21:56:34 +0100

    Added new class for testing page with actions. Added test about moving cursor on elements

---
+++ src/test/java/ActionsTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ActionsTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome_v80/chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://przyklady.javastart.pl/test/hover_mouse.html");
+    }
+
+    @Test
+    public void makeImagesBiggerByMovingScrollOnElement(){
+        WebElement smileyIcon1 = driver.findElement(By.id("smiley"));
+        WebElement smileyIcon2 = driver.findElement(By.id("smiley2"));
+
+        Actions actions = new Actions(driver);
+
+        // chain of actions by dots. "build()" is provided for building actions and "perform()" to executing them
+        actions.moveToElement(smileyIcon1).moveToElement(smileyIcon2).build().perform();
+
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+
+}
