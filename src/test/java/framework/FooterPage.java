72806d95a515b7d669b11d29d44559e0ed1c81f9
c0dexter <michal.dobrowolski1@gmail.com>
2020-03-01 23:32:14 +0100

    Added Page Object for the footer area

--- src/test/java/framework/FooterPage.java
+++ src/test/java/framework/FooterPage.java
@@ -1,4 +1,22 @@
 package framework;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class FooterPage {
+    WebDriver driver;
+
+    @FindBy(css = "#Banner img[src*='dog']")
+    WebElement banner;
+
+    public FooterPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public Boolean isBannerAfterLoginDisplayed() {
+        return banner.isDisplayed();
+    }
 }
