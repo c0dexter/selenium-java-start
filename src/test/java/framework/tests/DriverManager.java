5bd30a85c5482a1864529bf4837af45ea7ce499d
c0dexter <michal.dobrowolski1@gmail.com>
2020-03-03 21:31:37 +0100

    Added DriverManager class (Singleton Pattern) to provide driver object with methods: getWebDriver() and disposeDriver()

---
+++ src/test/java/framework/tests/DriverManager.java
@@ -0,0 +1,29 @@
+package framework.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    public DriverManager() {
+    }
+
+    public static WebDriver getWebDriver() {
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome_v80/chromedriver");
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void disposeDriver() {
+        driver.close();
+        driver.quit();
+        driver = null;
+        //Przypisane wartości null jest konieczne, ponieważ w pamięci Javy po wykonaniu metody quit(),
+        // dalej znajduje się obiekt klasy WebDrivera, i musimy go po prostu z fizycznie usunąć.
+        // Inaczej przy kolejnym wywołaniu metody getDriver(),
+        // metoda nie zwróci nam nowego obiektu, lecz stary, który już wyłączył okno przeglądarki.
+    }
+}
--- src/test/java/framework/tests/TestBase.java
+++ src/test/java/framework/tests/TestBase.java
@@ -1,26 +1,24 @@
 package framework.tests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-    public WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome_v80/chromedriver");
-        driver = new ChromeDriver();
+        driver = DriverManager.getWebDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         driver.navigate().to("http://przyklady.javastart.pl/jpetstore/");
     }
 
     @AfterMethod
     public void afterTest() {
-        driver.close();
-        driver.quit();
+        DriverManager.disposeDriver();
     }
 }
