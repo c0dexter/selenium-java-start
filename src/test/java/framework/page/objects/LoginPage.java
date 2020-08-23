b3e5472beb82935fe75f6eafbf8b7ea484237051
c0dexter <michal.dobrowolski1@gmail.com>
2020-08-23 23:07:56 +0200

    Added test logging function into already existed tests

--- src/test/java/framework/page/objects/LoginPage.java
+++ src/test/java/framework/page/objects/LoginPage.java
@@ -2,11 +2,16 @@ package framework.page.objects;
 
 import framework.driver.manager.DriverManager;
 import framework.waits.WaitForElement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+
+    // Logowanie przebigu testu - inicjalizowanie loggera
+    private Logger logger = LogManager.getRootLogger();
 
     // Page Factory
     @FindBy(name = "username")
@@ -37,24 +42,29 @@ public class LoginPage {
         WaitForElement.waitUntilElementIsVisible(usernameField);
         usernameField.clear();
         usernameField.sendKeys(username);
+        logger.info("Typed into User Name Field {}", username);
     }
 
     public void typeIntoPasswordField(String password) {
         passwordField.clear();
         passwordField.sendKeys(password);
+        logger.info("Typed into Password Field {}", password);
     }
 
     public void clickOnLoginButton() {
         signOnButton.click();
+        logger.info("Clicked on Login Button");
     }
 
     public String getWarningMessage() {
         WaitForElement.waitUntilElementIsVisible(messageLabel);
+        logger.info("Returned warning message was: {}", messageLabel);
         return messageLabel.getText();
     }
 
     public void clickOnFish() {
         fishButton.click();
+        logger.info("Clicked on Fish Button");
     }
 
     public void clickOnDogs() {
