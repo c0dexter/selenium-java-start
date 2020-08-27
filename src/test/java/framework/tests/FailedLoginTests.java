0032ba5baca1478590d84d692da4730980209b8f
c0dexter <michal.dobrowolski1@gmail.com>
2020-08-27 22:47:03 +0200

    Changed FailedLoginTests, now is using realtive URL

--- src/test/java/framework/tests/FailedLoginTests.java
+++ src/test/java/framework/tests/FailedLoginTests.java
@@ -1,16 +1,20 @@
 package framework.tests;
 
+import framework.driver.manager.DriverUtils;
 import framework.page.objects.LandingPage;
 import framework.page.objects.LoginPage;
 import framework.page.objects.TopMenuPage;
 import org.testng.annotations.Test;
 
+import static framework.navigation.ApplicationURLs.LOGIN_URL;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class FailedLoginTests extends TestBase {
 
     @Test
     public void asUserTryToLogInWithIncorrectLoginAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
         LandingPage landingPage = new LandingPage();
         landingPage.clickOnEnterStoreLink();
 
