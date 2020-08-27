c0d4a2a4fe3f541d9ff0b9cc984e4518e18045e0
c0dexter <michal.dobrowolski1@gmail.com>
2020-08-27 22:47:23 +0200

    Changed PositiveLoginTests, now is using realtive URL

--- src/test/java/framework/tests/PositiveLoginTests.java
+++ src/test/java/framework/tests/PositiveLoginTests.java
@@ -1,14 +1,18 @@
 package framework.tests;
 
+import framework.driver.manager.DriverUtils;
 import framework.page.objects.LandingPage;
 import org.testng.annotations.Test;
 
+import static framework.navigation.ApplicationURLs.LOGIN_URL;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class PositiveLoginTests extends TestBase {
 
     @Test
     public void asUserTryToLogInWithCorrectLoginAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
         LandingPage landingPage = new LandingPage();
 
         boolean isBannerAfterLoginDisplayed = landingPage
