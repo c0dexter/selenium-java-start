d62ad86d3a4ea2b756a6073f07787332f58f2879
c0dexter <michal.dobrowolski1@gmail.com>
2020-08-30 22:02:48 +0200

    Changed BrowserFactory class to using configuration from properties

--- src/test/java/framework/driver/BrowserFactory.java
+++ src/test/java/framework/driver/BrowserFactory.java
@@ -1,5 +1,6 @@
 package framework.driver;
 
+import configuration.LocalWebDriverProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,10 +10,10 @@ public class BrowserFactory {
     public static WebDriver getBrowser(BrowserType browserType) {
         switch (browserType) {
             case CHROME:
-                System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome_v80/chromedriver");
+                System.setProperty("webdriver.chrome.driver", LocalWebDriverProperties.getChromeWebDriverLocation());
                 return new ChromeDriver();
             case FIREFOX:
-                System.setProperty("webdriver.gecko.driver", "drivers/repository/firefox_0_26_0/geckodriver");
+                System.setProperty("webdriver.gecko.driver", LocalWebDriverProperties.getFirefoxWebDriverLocation());
                 return new FirefoxDriver();
             default:
                 throw new IllegalStateException("Unknown browser type! Please check your configuration");
