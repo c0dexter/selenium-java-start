02b996f1af4aae7b823a24d7875fe2b8681d91bc
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-11 18:12:37 +0200

    Added the method setWebDriver in DriverManager class

--- src/test/java/framework/driver/manager/DriverManager.java
+++ src/test/java/framework/driver/manager/DriverManager.java
@@ -10,13 +10,42 @@ import static configuration.TestRunProperties.getIsRemoteRun;
 public class DriverManager {
 
     private static ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
+    private static ThreadLocal<BrowserType> browserTypeThreadLocal = new ThreadLocal<>();
 
     private DriverManager() {
     }
 
+    public static void setWebDriver(BrowserType browserType){
+
+        //Obiekt typu WebDriver, który w kolejnych liniach zostanie zainicjalizowany wywołaniem metody getBrowser() z klasy BrowserFactory
+        WebDriver browser = null;
+
+        //Jeśli obiekt browserType będzie nullem, wtedy dla danego wątku zostanie wybrana przeglądarka zdefiniowana
+        // w pliku configuration.properties
+        if (browserType == null){
+
+            //Utworzenie instancji WebDrivera dla opcji gdy browserType jest nullem
+            //Zostanie wtedy wybrana przeglądarka zdefiniowana w pliku configuration.properties
+            browserType = getBrowserTypeToRun();
+            browser = new BrowserFactory(browserType, getIsRemoteRun()).getBrowser();
+        } else {
+            //Utworzenie instancji WebDrivera dla opcji gdy browserType nie jest nullem
+            //To znaczy, że został on zdefiniowany w pliku TestNG XML i możemy go używać
+            browser = new BrowserFactory(browserType, getIsRemoteRun()).getBrowser();
+        }
+
+        //Dodanie do puli instancji ThreadLocal za pomocą metody set() instancji klasy BrowserType
+        browserTypeThreadLocal.set(browserType);
+
+        //Dodanie do puli instancji ThreadLocal za pomocą metody set() instancji klasy WebDriver
+        webDriverThreadLocal.set(browser);
+    }
+
     public static WebDriver getWebDriver() {
         if (webDriverThreadLocal.get() == null) {
-            webDriverThreadLocal.set(new BrowserFactory(getBrowserTypeToRun(), getIsRemoteRun()).getBrowser());
+            // Rzucenie wyjątku IllegalStateException w sytuacji gdy dla danego wątku instancja przeglądarki nie została
+            // poprawnie zainicializowana metodą setWebDriver
+            throw new IllegalStateException("WebDriver Instance was null! Please create instance of WebDriver using setWebDriver!");
         }
         return webDriverThreadLocal.get();
     }
