e621087b95f0bf9fd9b49fc942fcef108e91de99
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-14 23:30:40 +0200

    Added and implemented class WebDriverEventListenerRegistrar

--- src/test/java/framework/driver/listeners/WebDriverEventListenerRegistrar.java
+++ src/test/java/framework/driver/listeners/WebDriverEventListenerRegistrar.java
@@ -1,2 +1,13 @@
-package framework.driver.listeners;public class WebDriverEventListenerRegistrar {
+package framework.driver.listeners;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+public class WebDriverEventListenerRegistrar {
+
+    public synchronized static WebDriver registerWebDriverEventListener(WebDriver driver) {
+        EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(driver);
+        DriverEventListener driverEventListener = new DriverEventListener();
+        return eventFiringWebDriver.register(driverEventListener);
+    }
 }
