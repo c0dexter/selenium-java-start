4ae87c2f6837d5c18c5a3a45f5691b10f36a03db
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-14 18:02:54 +0200

    Added @Step annotations in the TestBase class

--- src/test/java/framework/tests/TestBase.java
+++ src/test/java/framework/tests/TestBase.java
@@ -5,6 +5,7 @@ import configuration.PropertiesLoader;
 import framework.driver.BrowserType;
 import framework.driver.manager.DriverManager;
 import framework.driver.manager.DriverUtils;
+import io.qameta.allure.Step;
 import org.testng.annotations.*;
 
 import java.util.Properties;
@@ -13,6 +14,7 @@ import static framework.navigation.ApplicationURLs.APPLICATION_URL;
 
 public class TestBase {
 
+    @Step("Loading configuration from configuration.properties")
     @BeforeClass
     public void beforeClass() {
         PropertiesLoader propertiesLoader = new PropertiesLoader();
@@ -20,6 +22,7 @@ public class TestBase {
         ConfigurationProperties.setProperties(propertiesFromFile);
     }
 
+    @Step("Setting up browser to: {browserType} and navigating to Home Page")
     @Parameters("browser") // Zaciągane z all_test_suite.xml
     @BeforeMethod
     public void beforeTest(@Optional BrowserType browserType) { // Optional, bo test może NIE być odpalony z XMLa)
@@ -29,6 +32,7 @@ public class TestBase {
         DriverUtils.navigateToPage(APPLICATION_URL);
     }
 
+    @Step("Disposing browser")
     @AfterMethod
     public void afterTest() {
         DriverManager.disposeDriver();
