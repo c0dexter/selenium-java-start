6371138b95718d8358d65a43692eeb59044f7c1c
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-14 18:22:29 +0200

    Added @Severity annotations in FailedLoginTests and PositiceLoginTests classes

--- src/test/java/framework/tests/FailedLoginTests.java
+++ src/test/java/framework/tests/FailedLoginTests.java
@@ -3,6 +3,8 @@ package framework.tests;
 import framework.driver.manager.DriverUtils;
 import framework.page.objects.LoginPage;
 import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
 import static framework.navigation.ApplicationURLs.LOGIN_URL;
@@ -10,6 +12,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class FailedLoginTests extends TestBase {
 
+    @Severity(SeverityLevel.NORMAL)
     @Test
     @Description("The goal of this test is to log in using not proper username and password" +
             " and check if warning message Invalid username or password is displayed")
--- src/test/java/framework/tests/PositiveLoginTests.java
+++ src/test/java/framework/tests/PositiveLoginTests.java
@@ -3,6 +3,8 @@ package framework.tests;
 import framework.driver.manager.DriverUtils;
 import framework.page.objects.LoginPage;
 import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
 import static framework.navigation.ApplicationURLs.LOGIN_URL;
@@ -10,6 +12,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class PositiveLoginTests extends TestBase {
 
+    @Severity(SeverityLevel.BLOCKER)
     @Test
     @Description("The goal of this test is to log in using proper username and password" +
             " and check if Dog Banner is displayed after")
