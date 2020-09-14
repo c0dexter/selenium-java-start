f965ca26cdc6ed339c73b226e8b2817d76f339f6
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-14 18:01:07 +0200

    Added @Description annotation for providing description for tests which will be visible in report

--- src/test/java/framework/tests/PositiveLoginTests.java
+++ src/test/java/framework/tests/PositiveLoginTests.java
@@ -2,6 +2,7 @@ package framework.tests;
 
 import framework.driver.manager.DriverUtils;
 import framework.page.objects.LoginPage;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 import static framework.navigation.ApplicationURLs.LOGIN_URL;
@@ -10,6 +11,8 @@ import static org.testng.AssertJUnit.assertTrue;
 public class PositiveLoginTests extends TestBase {
 
     @Test
+    @Description("The goal of this test is to log in using proper username and password" +
+            " and check if Dog Banner is displayed after")
     public void asUserTryToLogInWithCorrectLoginAndPassword() {
         DriverUtils.navigateToPage(LOGIN_URL);
 
