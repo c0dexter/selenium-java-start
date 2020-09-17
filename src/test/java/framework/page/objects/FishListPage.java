9df21a039561e8bf18516fbe94efd62931f3fd80
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-17 23:47:01 +0200

    Refactored FishListPage, extends BasePage. Changed calling logger function

--- src/test/java/framework/page/objects/FishListPage.java
+++ src/test/java/framework/page/objects/FishListPage.java
@@ -2,15 +2,11 @@ package framework.page.objects;
 
 import framework.driver.manager.DriverManager;
 import framework.waits.WaitForElement;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class FishListPage {
-
-    private Logger logger = LogManager.getLogger(FishListPage.class);
+public class FishListPage extends BasePage{
 
     //Page Object
     @FindBy(css = "a[href$='FI-SW-01']")
@@ -29,25 +25,25 @@ public class FishListPage {
     public AngelFishListPage clickOnAngelFishProductId() {
         WaitForElement.waitUntilElementIsClickable(angelfishProductId);
         angelfishProductId.click();
-        logger.info("Clicked on Small Angel Fish Product ID");
+        log().info("Clicked on Small Angel Fish Product ID");
         return new AngelFishListPage();
     }
 
     public void clickOnTigerSharkProductId() {
         WaitForElement.waitUntilElementIsClickable(angelfishProductId);
         tigerSharkProductId.click();
-        logger.info("Clicked on Tiger Shark Product ID");
+        log().info("Clicked on Tiger Shark Product ID");
     }
 
     public void clickOnKoiProductId() {
         WaitForElement.waitUntilElementIsClickable(angelfishProductId);
         koiProductId.click();
-        logger.info("Clicked on Koi Product ID");
+        log().info("Clicked on Koi Product ID");
     }
 
     public void clickOnGoldFishProductId() {
         WaitForElement.waitUntilElementIsClickable(angelfishProductId);
         goldfishProductId.click();
-        logger.info("Clicked on Goldfish Product ID");
+        log().info("Clicked on Goldfish Product ID");
     }
 }
