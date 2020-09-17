7a99a25f0287ac882f1e364608d3a6acd9297772
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-17 23:50:56 +0200

    Refactored LandingPage, extends BasePage. Changed calling logger function

--- src/test/java/framework/page/objects/LandingPage.java
+++ src/test/java/framework/page/objects/LandingPage.java
@@ -1,32 +1,22 @@
 package framework.page.objects;
 
-import framework.driver.manager.DriverManager;
 import framework.waits.WaitForElement;
 import io.qameta.allure.Step;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-// Page Object of first page
-public class LandingPage {
 
-    private Logger logger = LogManager.getLogger(LandingPage.class);
+public class LandingPage extends BasePage{
 
     // PageFactory
     @FindBy(css = "#Content a")
     WebElement enterStoreLink;
 
-    public LandingPage(){
-        PageFactory.initElements(DriverManager.getWebDriver(), this); // Initializing elements in the constructor
-    }
-
     @Step("Click on Enter Store link")
     public TopMenuPage clickOnEnterStoreLink() {
         WaitForElement.waitUntilElementIsClickable(enterStoreLink);
         enterStoreLink.click();
-        logger.info("Clicked on Enter Store link");
+        log().info("Clicked on Enter Store link");
         return new TopMenuPage();
     }
 }
