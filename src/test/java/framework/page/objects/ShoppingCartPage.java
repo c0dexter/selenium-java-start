ad3bc6df7d16c81fd2f5d1d0cd9aec13fa42c89f
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-18 00:12:32 +0200

    Refactored ShoppingCartPage, extends BasePage. Changed calling logger function

--- src/test/java/framework/page/objects/ShoppingCartPage.java
+++ src/test/java/framework/page/objects/ShoppingCartPage.java
@@ -1,27 +1,17 @@
 package framework.page.objects;
 
-import framework.driver.manager.DriverManager;
 import framework.waits.WaitForElement;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-public class ShoppingCartPage {
-
-    private Logger logger = LogManager.getLogger(ShoppingCartPage.class);
+public class ShoppingCartPage extends BasePage {
 
     @FindBy(css = "a[href$='newOrderForm=']")
     WebElement proceedToCheckoutButton;
 
-    public ShoppingCartPage() {
-        PageFactory.initElements(DriverManager.getWebDriver(), this); // Initializing elements in the constructor
-    }
-
     public void clickProceedToCheckoutButton() {    // TODO: tu będzie zwrócenie wartości CheckoutPage dla pozytywnego testu (nie ma dodanego takiego PageObject jeszcze)
         WaitForElement.waitUntilElementIsClickable(proceedToCheckoutButton);
         proceedToCheckoutButton.click();
-        logger.info("Clicked on the Proceed To Checkout Button");
+        log().info("Clicked on the Proceed To Checkout Button");
     }
 }
