5939bc3aa78b4fbb9a5e7a641a469f6a6a5436e6
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-17 23:53:45 +0200

    Refactored LoginPage, extends BasePage. Changed calling logger function

--- src/test/java/framework/page/objects/LoginPage.java
+++ src/test/java/framework/page/objects/LoginPage.java
@@ -1,20 +1,13 @@
 package framework.page.objects;
 
-import framework.driver.manager.DriverManager;
 import framework.generic.assertions.AssertWebElement;
 import framework.waits.WaitForElement;
 import io.qameta.allure.Step;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
-
-    // Logowanie przebigu testu - inicjalizowanie loggera
-    private Logger logger = LogManager.getLogger(LoginPage.class);
-
+public class LoginPage extends BasePage{
+    
     // Page Factory
     @FindBy(name = "username")
     WebElement usernameField;
@@ -35,17 +28,12 @@ public class LoginPage {
     @FindBy(css = "#SidebarContent a[href*='Id=BIRDS']")
     WebElement birdsButton;
 
-
-    public LoginPage() {
-        PageFactory.initElements(DriverManager.getWebDriver(), this); // Initializing elements in the constructor
-    }
-
     @Step("Type into User Name Field {username}")
     public LoginPage typeIntoUserNameField(String username) {
         WaitForElement.waitUntilElementIsVisible(usernameField);
         usernameField.clear();
         usernameField.sendKeys(username);
-        logger.info("Typed into User Name Field {}", username);
+        log().info("Typed into User Name Field {}", username);
         return this;
     }
 
@@ -53,20 +41,20 @@ public class LoginPage {
     public LoginPage typeIntoPasswordField(String password) {
         passwordField.clear();
         passwordField.sendKeys(password);
-        logger.info("Typed into Password Field {}", password);
+        log().info("Typed into Password Field {}", password);
         return this;
     }
 
     @Step("Click on Login Button")
     public FooterPage clickOnLoginButton() {
         signOnButton.click();
-        logger.info("Clicked on Login Button");
+        log().info("Clicked on Login Button");
         return new FooterPage();
     }
 
     @Step("Getting warning message from Login Page")
     public LoginPage assertThatWarningIsDisplayed(String warningMessage) {
-        logger.info("Checking if warning message {} is displayed", warningMessage);
+        log().info("Checking if warning message {} is displayed", warningMessage);
         WaitForElement.waitUntilElementIsVisible(messageLabel);
         AssertWebElement.assertThat(messageLabel).isDisplayed().hasText(warningMessage);
         return this;
@@ -76,7 +64,7 @@ public class LoginPage {
     public FishListPage clickOnFish() {
         WaitForElement.waitUntilElementIsClickable(fishButton);
         fishButton.click();
-        logger.info("Clicked on Fish Button");
+        log().info("Clicked on Fish Button");
         return new FishListPage();
     }
 
